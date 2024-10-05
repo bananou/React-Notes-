17668af31963804d41873553cc1a9a549e07d0de
bananou <77001808+bananou@users.noreply.github.com>
2024-10-05 11:20:59 +0100

    Created project structure with CreateNote, EditNote, and Notes pages. Added basic interfaces and styles.

---
+++ src/pages/CreateNote.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { IoIosArrowBack } from "react-icons/io";
+
+const CreateNote = () => {
+  return (
+    <section>
+      <header className="create-note__header">
+        <Link to="/" className="btn">
+          <IoIosArrowBack />
+        </Link>
+        <button className="btn lg primary">Save</button>
+      </header>
+      <form className="create-note__form ">
+        <input type="text" placeholder="Title" autoFocus></input>
+        <textarea rows="28" placeholder="Note details ..."></textarea>
+      </form>
+    </section>
+  );
+};
+
+export default CreateNote;
